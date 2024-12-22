3bab414129ca7aa3eee4b9e8fb19b68d7956c942
vigneshwaran-star <nanbanenemy@143@gmail.com>
2024-12-22 18:49:30 +0530

    add commit with _headers

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,7 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:'https://vigneshwaran-star.github.io/Squad76Portfolio/'
+// })
+
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
-  base:'https://vigneshwaran-star.github.io/Squad76Portfolio/'
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_headers',
+          dest: './', // Copies _headers to the root of the dist directory
+        },
+      ],
+    }),
+  ],
+});
